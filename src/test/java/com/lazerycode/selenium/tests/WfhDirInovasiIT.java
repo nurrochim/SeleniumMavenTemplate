a06f592b1c34f1cda85ca3864edc1f8a53a6fabf
nurrochim <masnur.bangett@gmail.com>
2020-09-12 13:37:43 +0700

    add some condition for DirInovasiIT

--- src/test/java/com/lazerycode/selenium/tests/WfhDirInovasiIT.java
+++ src/test/java/com/lazerycode/selenium/tests/WfhDirInovasiIT.java
@@ -14,27 +14,38 @@ public class WfhDirInovasiIT extends DriverBase2 {
 		Calendar cal = Calendar.getInstance();
     	int currentHour = cal.get(Calendar.HOUR_OF_DAY);
     	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    	String dateNow = sdf.format(cal.getTime()); 
+    	String dateNow = sdf.format(cal.getTime());
+    	// group Direktorat Inovasi
+//    	wfhService.setFindByChatByGroupName("Inovasi Industri");
+
     	
 		WebDriver driver = getDriver();
     	WfhService wfhService = new WfhService();
     	wfhService.setDriver(driver);
-    	if (currentHour >= 20 || (currentHour > 8 && currentHour < 12)) {
-    		wfhService.setPesanWfhWfoDinas(false);
-    	}
-    	
-    	// group Direktorat Inovasi
-//    	wfhService.setFindByChatByGroupName("Inovasi Industri");
     	
     	System.out.println("Jam running "+cal.getTime()+"  "+ currentHour);
+    	if (currentHour >= 18 || (currentHour > 9 && currentHour < 12)) {
+    		// HANYA PESAN YANG BELUM CICO SAJA, YANG SUDAH CICO GA USAH TAMPIL
+    		wfhService.setPesanWfhWfoDinas(false);
+    		wfhService.setPesanBelumCiCo(true);
+    	}
+    	if (currentHour < 7 || (currentHour > 14 && currentHour < 16)) {
+    		// HANYA PESAN YANG SUDAH CICO SAJA, YANG BELUM CICO GA USAH TAMPIL
+    		wfhService.setPesanWfhWfoDinas(true);
+    		wfhService.setPesanBelumCiCo(false);
+    	}
+    	
+    	
     	if (currentHour < 12) {
     		wfhService.setKey("Checkin");
     	}else {
     		wfhService.setKey("Checkout");
     	}
-    	//if (currentHour == 6 && dateNow.equals("14/09/2020")) {
-		if (currentHour == 6) {
+    	//if (currentHour == 5 && dateNow.equals("14/09/2020")) {
+		if (currentHour == 5) {
     		wfhService.setPesanDisclaimer(true);
+    		wfhService.setPesanWfhWfoDinas(false);
+    		wfhService.setPesanBelumCiCo(false);
     	}
     	
     	wfhService.wfhHistory(wfhService.getPersonsName1());
